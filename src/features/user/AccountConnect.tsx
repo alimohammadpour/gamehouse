b1102703235cbac0d2b22362b8f1c086e19538e7
alimohammadpour <alimpr027@gmail.com>
2025-06-14 04:16:15 +0200

    adds AccountConnect styles

--- src/features/user/AccountConnect.tsx
+++ src/features/user/AccountConnect.tsx
@@ -2,6 +2,7 @@ import React, { useState, type JSX } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { requestCodePending } from './userSlice';
+import { FeaturesBanner } from '../../components/FeaturesBanner';
 
 export const AccountConnect = (): JSX.Element => {
   const dispatch = useDispatch();
@@ -18,25 +19,29 @@ export const AccountConnect = (): JSX.Element => {
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
 
   return (
-    <div className="step step1">
-      <h1>Connect Your Account</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder="Enter your email"
-          required
-        />
-        <label className="flex items-center">
-            <input
-            type="checkbox"
-            defaultChecked
-            />
-            Send Me Offers, News, and Fun Stuff!
-        </label>
-        <button type="submit">Connect</button>
-      </form>
+    <div className='card'>
+      <FeaturesBanner />
+      <div className="step">
+        <h2>Connect Your Account</h2>
+        <p>...and unlock your benefits!</p>
+        <form onSubmit={handleFormSubmit} className='form'>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Email Address"
+            required
+          />
+          <label className="flex items-center">
+              <input
+              type="checkbox"
+              defaultChecked
+              />
+              Send Me Offers, News, and Fun Stuff!
+          </label>
+          <button type="submit">Connect</button>
+        </form>
+      </div>
     </div>
   );
 };
